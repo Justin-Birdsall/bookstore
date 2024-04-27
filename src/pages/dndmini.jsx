e14d93181f7bb78bab48447498044726c4d842de
Justin-Birdsall <Birdsaju@mail.gvsu.edugit config --global user.email Birdsaju@mail.gvsu.edugit config --global user.email Birdsaju@mail.gvsu.edu>
2024-04-26 21:06:18 -0400

    the name of the game commit then submit

--- src/pages/dndmini.jsx
+++ src/pages/dndmini.jsx
@@ -1,13 +1,49 @@
 
-import React, { Suspense, useRef } from 'react'
+import React, { Suspense, useRef, useState, useEffect } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Stage } from '@react-three/drei'
 import { Model } from '../components/Model'
 import Navbar from '../components/Navbar'
-export default function Viewer() {
-  return (<>
-<Navbar>
+import { Link } from 'react-router-dom'
 
+export default function Viewer() {
+
+    const [isNavbarInView, setIsNavbarInView] = useState(false);
+    const observer = useRef(null);
+  
+    useEffect(() => {
+      observer.current = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsNavbarInView(true);
+          } else {
+            setIsNavbarInView(false);
+          }
+        });
+      });
+  
+      observer.current.observe(document.querySelector('.navbar'));
+  
+      return () => {
+        if (observer.current) {
+          observer.current.disconnect();
+        }
+      };
+    }, []);
+
+  return (<>
+
+<Navbar>
+<div className="text-sm breadcrumbs">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <a>Dungeons & Dragons Mini</a>
+          </li>
+        </ul>
+      </div>
 </Navbar>
  <div className="flex justify-center items-center h-screen">
 
